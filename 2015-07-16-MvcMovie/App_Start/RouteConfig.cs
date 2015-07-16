a600337d276eda5e5b82bb5a876c5dcca3504d60
Yashmyn Jackson <yashmyn@gmail.com>
2015-07-16 16:04:03 -0400

    Part Two: Change to App_Start\RouteConfig.cs

--- 2015-07-16-MvcMovie/App_Start/RouteConfig.cs
+++ 2015-07-16-MvcMovie/App_Start/RouteConfig.cs
@@ -18,6 +18,11 @@ namespace _2015_07_16_MvcMovie
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
             );
+
+            routes.MapRoute(
+                name: "Hello",
+                url: "{controller}/{action}/{name}/{id}"
+                );
         }
     }
 }
