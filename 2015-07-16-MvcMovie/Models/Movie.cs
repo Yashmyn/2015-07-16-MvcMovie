9c8b51768e96dc6281f00cbe6bbcd702c5e804d2
Yashmyn Jackson <yashmyn@gmail.com>
2015-07-20 13:57:38 -0400

    Added second connection string

--- 2015-07-16-MvcMovie/Models/Movie.cs
+++ 2015-07-16-MvcMovie/Models/Movie.cs
@@ -1,9 +1,9 @@
 ﻿using System;
 using System.Data.Entity;
 
-namespace _2015_07_16_MvcMovie.Models
+namespace _2015_07_16_MvcMovie.Models  //created my first model
 {
-    public class Movie
+    public class Movie //created class Movie
     {
         public int ID { get; set; }
         public string Title { get; set; }
@@ -11,8 +11,8 @@ namespace _2015_07_16_MvcMovie.Models
         public string Genre { get; set; }
         public decimal Price { get; set; }
     }
-
-    public class MovieDBContext : DbContext
+    
+    public class MovieDBContext : DbContext  //created this database-container for items of type Movie
     {
         public DbSet<Movie> Movies { get; set; }
     }
