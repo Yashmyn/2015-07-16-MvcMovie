c909f887fcfad0f3c24b00d4121132de154be75f
Yashmyn Jackson <yashmyn@gmail.com>
2015-07-21 15:57:52 -0400

    Corrected an addition to the Movies Controller class index.

--- 2015-07-16-MvcMovie/Controllers/MoviesController.cs
+++ 2015-07-16-MvcMovie/Controllers/MoviesController.cs
@@ -24,7 +24,7 @@ namespace _2015_07_16_MvcMovie.Controllers
             {
                 movies = movies.Where(s => s.Title.Contains(searchString));
             }
-            return View(db.Movies.ToList());
+            return View(movies);
         }
 
         // GET: Movies/Details/5
