b770fb65412f3076812e73a49a5666aaf4d400ff
Yashmyn Jackson <yashmyn@gmail.com>
2015-07-22 15:38:56 -0400

    Part 09: Changes related to Rating

---
+++ 2015-07-16-MvcMovie/Migrations/201507221930414_Rating.cs
@@ -0,0 +1,18 @@
+namespace _2015_07_16_MvcMovie.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class Rating : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.Movies", "Rating", c => c.String());
+        }
+        
+        public override void Down()
+        {
+            DropColumn("dbo.Movies", "Rating");
+        }
+    }
+}
