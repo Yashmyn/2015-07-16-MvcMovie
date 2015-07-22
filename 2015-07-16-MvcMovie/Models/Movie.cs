0c7a7c10b0810c57a188454322a9af432c38ae33
Yashmyn Jackson <yashmyn@gmail.com>
2015-07-22 14:24:36 -0400

    Part 09: Added Rating to binding white list

--- 2015-07-16-MvcMovie/Models/Movie.cs
+++ 2015-07-16-MvcMovie/Models/Movie.cs
@@ -17,6 +17,7 @@ namespace _2015_07_16_MvcMovie.Models  //created my first model
         public DateTime ReleaseDate { get; set; }
         public string Genre { get; set; }
         public decimal Price { get; set; }
+        public string Rating { get; set; }
     }
     
     public class MovieDBContext : DbContext  //created this database-container for items of type Movie
