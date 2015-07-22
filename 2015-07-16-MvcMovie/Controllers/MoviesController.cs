eb2190a197480f42ed203482279b76d70b305b6a
Yashmyn Jackson <yashmyn@gmail.com>
2015-07-22 14:29:36 -0400

    Part 09: Added <th> for Rating in Views\Movies\Index

--- 2015-07-16-MvcMovie/Controllers/MoviesController.cs
+++ 2015-07-16-MvcMovie/Controllers/MoviesController.cs
@@ -66,7 +66,7 @@ namespace _2015_07_16_MvcMovie.Controllers
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create([Bind(Include = "ID,Title,ReleaseDate,Genre,Price,Rating")] Movie movie)
+        public ActionResult Create([Bind(Include = "ID,Title,ReleaseDate,Genre,Price,Rating")] Movie movie) //added Rating
         {
             if (ModelState.IsValid)
             {
